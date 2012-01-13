442edda6c26f7de9bc7ab583786238c87d66b29a
Daniel Berger <djberg96@gmail.com>
2012-01-12 18:10:47 -0700

    Bumped version test.

--- test/test_io_extra.rb
+++ test/test_io_extra.rb
@@ -20,7 +20,7 @@ class TC_IO_Extra < Test::Unit::TestCase
   end
 
   def test_version
-    assert_equal('1.2.6', IO::EXTRA_VERSION)
+    assert_equal('1.3.0', IO::EXTRA_VERSION)
   end
 
   def test_direct_constant
