43485559294a900b26910dcf25320e2808708b1b
Daniel Berger <djberg96@gmail.com>
2012-01-31 18:58:57 -0700

    Merge pread and pread_ptr calls into a single method by having it return the ffi pointer and letting users take it from there.

--- lib/io/extra.rb
+++ lib/io/extra.rb
@@ -7,6 +7,19 @@ class IO
   attach_function :pread_c, :pread, [:int, :pointer, :size_t, :off_t], :size_t
   attach_function :strerror, [:int], :string
 
+  # IO.pread(fd, length, offset)
+  #
+  # This method is based on the IO.read method except that it reads from
+  # the given offset in the file without modifying the file pointer. Also,
+  # the length and offset arguments are not optional.
+  #
+  # Returns an FFI::MemoryPointer. If you want the string, call ptr#read_string.
+  # If you want the memory address, call ptr#address.
+  #--
+  # The reason for returning a pointer instead of a plain string is that it's
+  # possible that the resulting buffer could be empty, in which casse it would
+  # be impossible to get the pointer address from the raw string.
+  #
   def self.pread(fd, length, offset)
     ptr = FFI::MemoryPointer.new(:void, length)
 
@@ -14,6 +27,6 @@ class IO
       raise "pread function failed: " + strerror(FFI.errno)
     end
 
-    ptr.read_string
+    ptr
   end
 end
