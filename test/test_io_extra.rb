8b6102e4c729f755fc08a0f77c5038015910f0f8
Daniel Berger <djberg96@gmail.com>
2012-03-06 04:39:47 -0700

    Added more tests.

--- test/test_io_extra.rb
+++ test/test_io_extra.rb
@@ -112,12 +112,26 @@ class TC_IO_Extra < Test::Unit::TestCase
     assert_equal("quick", @fh.pread(5,4).read_string)
   end
 
-  test "pread instance_method requires two integer arguments" do
+  test "pread instance method requires two integer arguments" do
     assert_raise(ArgumentError){ @fh.pread }
     assert_raise(ArgumentError){ @fh.pread(1) }
     assert_raise(ArgumentError){ @fh.pread(1,2,3) }
     assert_raise(TypeError){ @fh.pread(5, 'test') }
   end
+
+  # TODO: Fix this failure
+  #test "pread instance method works in binary mode" do
+  #  @fh.binmode
+  #  size = @fh.stat.size
+  #  @fh.syswrite("FOO\0HELLO")
+  #  assert_equal("O\0H", @fh.pread(3, size + 2).read_string)
+  #end
+
+  # TODO: Fix this failure
+  #def test_pread_last
+  #  size = @fh.stat.size
+  #  assert_equal("ck\n", @fh.pread(5, size - 3).read_string)
+  #end
 
   test "pread singleton method basic functionality" do
     assert_respond_to(IO, :pread)
@@ -137,42 +151,59 @@ class TC_IO_Extra < Test::Unit::TestCase
     assert_raise(TypeError){ IO.pread(@fh.fileno, 5, 'test') }
   end
 
+  test "pwrite instance method basic functionality" do
+    assert_respond_to(@fh, :pwrite)
+    assert_nothing_raised{ @fh.pwrite("HAL", 0) }
+  end
+
+  test "pwrite instance method returns the number of bytes written" do
+    assert_equal(3, @fh.pwrite("HAL", 0))
+  end
+
+  # TODO: Fix this failure
+  #test "pwrite instance method writes data as expected" do
+  #  @fh.pwrite("HAL", 0)
+  #  @fh.rewind
+  #  assert_equal("HAL", @fh.read(3))
+  #end
+
+  test "pwrite instance method requires two arguments only" do
+    assert_raise(ArgumentError){ @fh.pwrite }
+    assert_raise(ArgumentError){ @fh.pwrite("HAL") }
+    assert_raise(ArgumentError){ @fh.pwrite("HAL", 0, 0) }
+  end
+
+  test "pwrite instance method requires a string buffer and integer offset" do
+    assert_raise(ArgumentError){ @fh.pwrite(0, 0) }
+    assert_raise(TypeError){ @fh.pwrite("HAL", true) }
+  end
+
+  test "writev instance method basic functionality" do
+    assert_respond_to(@fh, :writev)
+    assert_nothing_raised{ @fh.writev(%w[hello world]) }
+  end
+
+  test "writev instance method returns the number of bytes written" do
+    assert_equal(10, @fh.writev(%w[hello world]))
+  end
+
+  test "writev instance method raises an error if array contains more than IOV_MAX members" do
+    arr = []
+    (IO::IOV_MAX + 1).times{ |n| arr << 'test' }
+    assert_raise(ArgumentError){ @fh.writev(arr) }
+  end
+
+  test "writev instance method requires one argument only" do
+    assert_raise(ArgumentError){ @fh.writev }
+    assert_raise(ArgumentError){ @fh.writev(['test'],['test']) }
+  end
+
+  test "writev instance method requires an array argument" do
+    assert_raise(TypeError){ @fh.writev(1) }
+    assert_raise(TypeError){ @fh.writev(true) }
+  end
+
 =begin
-  def test_pread_binary
-    @fh.close rescue nil
-    @fh = File.open(@file, "ab")
-    @fh.binmode
-    size = @fh.stat.size
-    assert_nothing_raised { @fh.syswrite("FOO\0HELLO") }
-    @fh.close rescue nil
-    @fh = File.open(@file)
-    assert_equal("O\0H", IO.pread(@fh.fileno, 3, size + 2))
-  end
-
-  def test_pread_ptr
-    @fh.close rescue nil
-    @fh = File.open(@file)
-    assert_respond_to(IO, :pread_ptr)
-    assert_kind_of(Integer, IO.pread_ptr(@fh.fileno, 5, 4))
-  end
-
-  def test_pread_last
-    @fh.close rescue nil
-    @fh = File.open(@file)
-    size = @fh.stat.size
-    assert_equal("ck\n", IO.pread(@fh.fileno, 5, size - 3))
-  end
-
-  def test_pwrite
-    assert_respond_to(IO, :pwrite)
-    assert_nothing_raised{ IO.pwrite(@fh.fileno, "HAL", 0) }
-  end
-
-  def test_writev
-    assert_respond_to(IO, :writev)
-    assert_equal(10, IO.writev(@fh.fileno, %w(hello world)))
-  end
-
   def test_writev_retry
     empty = ""
     if empty.respond_to?(:force_encoding)
@@ -206,13 +237,13 @@ class TC_IO_Extra < Test::Unit::TestCase
        end
     end
   end
+=end
 
-  def test_ttyname
+  test "ttyname basic functionality" do
     assert_respond_to(@fh, :ttyname)
     assert_nil(@fh.ttyname)
     assert_kind_of(String, STDOUT.ttyname)
   end
-=end
 
   def teardown
     @fh.close rescue nil
