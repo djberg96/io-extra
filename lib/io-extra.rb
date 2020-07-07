1024d1169739bfa34fafe6d55039ffa08ab34575
Daniel Berger <djberg96@gmail.com>
2020-07-07 07:45:35 -0400

    Get rid of stuff now implemented in core Ruby.

--- lib/io-extra.rb
+++ lib/io-extra.rb
@@ -1,1 +1,15 @@
 require 'io/extra'
+
+class IO
+  # Singleton version of the IO#pwrite method.
+  #
+  def self.pwrite(fd, string, offset)
+    fd.pwrite(string, offset)
+  end
+
+  # Singleton version of the IO#pread method.
+  #
+  def self.pread(fd, maxlen, offset)
+    fd.pread(maxlen, offset)
+  end
+end
